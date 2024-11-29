33978dffda8fed61f8e3802ee6a8674fc36a8c05
beto <beto_vicio@hotmail.com>
2024-11-29 16:46:19 +0000

    loaded textures and added to the shader uniforms

--- src/shaders/earth/fragment.glsl
+++ src/shaders/earth/fragment.glsl
@@ -7,6 +7,8 @@ void main()
     vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 normal = normalize(vNormal);
     vec3 color = vec3(vUv, 1.0);
+    vec3 secondColor = vec3(0.0,0.0,0.0);
+    color = mix(color,secondColor,normal);
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
